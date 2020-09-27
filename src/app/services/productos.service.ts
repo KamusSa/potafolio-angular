419d73670bfa5adafe06a27e08b41895f1bbc7e1
acluis <luis.aguilarcervantes@gnp.com.mx>
2020-09-27 16:05:26 -0500

    Proyecto termindo

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,20 +9,58 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = []; 
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-a7e3e.firebaseio.com/productos_idx.json')
-    .subscribe((resp: Producto[]) => {
-      console.log(resp);
-      this.productos = resp;
 
-      setTimeout(() =>{
+    return new Promise(  (resolve, reject) =>{
+
+      this.http.get('https://angular-html-a7e3e.firebaseio.com/productos_idx.json')
+      .subscribe((resp: Producto[]) => {
+        // console.log(resp);
+        this.productos = resp;
         this.cargando = false;
-      }, 3000);
+        resolve();
+      });
+    });
+  }
+
+  getProducto(id: string){
+    return this.http.get(`https://angular-html-a7e3e.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto(termino: string){
+
+    if ( this.productos.length === 0 ){
+      //Cargar Productos
+      this.cargarProductos().then(() => {
+        // Despues de obtener los productos
+        // Aplicar el filtro
+        this.filtrarProductos( termino );
+      });
+    }else{
+      // Aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+  }
+
+  filtrarProductos(termino: string){
+    console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLowerCase = prod.titulo.toLowerCase();
+
+      if ( prod.categoria.indexOf( termino) >= 0 || tituloLowerCase.indexOf( termino) >= 0 ){
+        this.productosFiltrado.push(prod);
+      }
     });
   }
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { InfoPaginaService } from '../../services/info-pagina.service';
 
 
@@ -9,9 +10,19 @@ import { InfoPaginaService } from '../../services/info-pagina.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public service: InfoPaginaService) { }
+  constructor(public service: InfoPaginaService,
+              private router: Router ) { }
 
   ngOnInit() {
+  }
+
+  buscarProducto(termino: string){
+    if (termino.length < 1){
+      return;
+    }
+    this.router.navigate(['/search', termino]);
+    // console.log(termino);
+
   }
 
 }
